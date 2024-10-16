bc6a3b568fcd39d6f7c766be2591b98a81c0968d
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-16 17:10:11 +0600

    complete module 38

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './Todo'
+import Actor from './Actor'
+import Singer from './Singer'
+import BookStore from './bookStore'
+import Book from './book'
 
 function App() {
 
+
+  const actors = ['sakib', 'raj', 'jasim', 'rubel', 'salman shah'];
+
+  const books = [
+    {id:1, name: 'physics', price: 100},
+    {id:1, name: 'biology', price: 120},
+    {id:1, name: 'chemistry', price: 130},
+    {id:1, name: 'math', price: 100},
+  ]
+
+  const singers = [
+    { id:1, name: 'dr. Mahfuzur rahman', age: 6},
+    { id:2, name: 'eva rahman', age: 45},
+    { id:3, name: 'shovra dev', age: 64},
+    { id:3, name: 'pritom', age: 64},
+  ]
   return (
     <>
       <h1>Vite + React</h1>
-      <Todo 
+
+    <BookStore books={books}></BookStore>
+
+    {
+      singers.map(singer => <Singer singer={singer}></Singer>)
+    }
+
+      <Actor name={'Bappa Raz'}></Actor>
+    {
+      actors.map(actor => <Actor name={actor}></Actor>)
+    }
+
+      {/* <Todo 
       task='learn react' 
       isDone='true'></Todo>
       
@@ -19,7 +51,7 @@ function App() {
       
       <Todo 
       task='try jsx' 
-      isDone='true'></Todo>
+      isDone='true'></Todo> */}
 
     {/* <Device name='laptop' price='55'></Device>
     <Device name='mobile' price='17'></Device>
